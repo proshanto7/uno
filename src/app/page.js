33ff8d41911cab59c182a32add8909ff3d149b9a
proshanto7 <proshantosarker112@gmail.com>
2026-06-16 01:58:43 +0600

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function page() {
-  return <div className="text-3xl bg-red-500">page</div>;
+  return <div className="text-3xl bg-red-500">
+
+
+<h1 className="text-red-600 text-6xl bg-green-500">hello</h1>
+
+
+  </div>;
 }
 
 export default page;
