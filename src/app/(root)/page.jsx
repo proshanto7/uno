a0358b4c39263936db5dd562e7545f23cd9b5647
proshanto7 <proshantosarker112@gmail.com>
2026-06-27 21:00:56 +0600

    discount add

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,6 +1,7 @@
 import Banner from '@/components/layout/home/Banner'
 import Category from '@/components/layout/home/category/Category'
 import Content from '@/components/layout/home/content/Content'
+import Discount from '@/components/layout/home/discount/Discount'
 import FeaturedProducts from '@/components/layout/home/product/FeaturedProducts'
 import Topselling from '@/components/layout/home/topSelling/Topselling'
 import React from 'react'
@@ -13,6 +14,7 @@ function page() {
       <FeaturedProducts/>
       <Category/>
       <Topselling/>
+      <Discount/>
       
     </main>
   )
---
+++ src/components/layout/home/discount/Discount.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "../../Container";
+import Title from "../Title";
+
+function Discount() {
+  return (
+    <section>
+      <Container>
+        <Title title="Discount" />
+      </Container>
+    </section>
+  );
+}
+
+export default Discount;
